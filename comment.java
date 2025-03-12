ea4928e302e0dd8c4bd69c1d9ea301a6290694f8
nandhanasg <nandhana26112003>
2025-03-12 14:43:08 +0530

    comment

---
+++ comment.java
@@ -0,0 +1,10 @@
+public class comment
+{
+    public static void main(String[] args) {
+        System.out.println("All is well");
+        System.out.print("Old is Gold"); //old is gold
+        System.out.println("All is beautiful");
+        System.out.println("India is my country");//india is my country
+    
+    }
+}
