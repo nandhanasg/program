311c72e9cb9d9a9de7c24d8a83f0e6b1b4c892c7
nandhanasg <nandhana26112003>
2025-03-13 12:10:51 +0530

    logic

---
+++ logic.java
@@ -0,0 +1,12 @@
+public class logic {
+    public static void main(String[] args) {
+        {
+            int a=10;
+            int b=20;
+            boolean c=a>b;
+            boolean d= a==b;
+            System.out.println((a>b)&&(a==b));
+            System.out.println((a<b)||(a==b));
+        }
+    }
+}
