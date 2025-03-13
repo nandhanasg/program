b1033b278800a7c082c97052e8b5a5ee97a48f70
nandhanasg <nandhana26112003>
2025-03-13 09:52:49 +0530

    student

---
+++ student.java
@@ -0,0 +1,12 @@
+public class student {
+    public static void main(String[] args) {
+        String name = "nandhana";
+        String subject="maths";
+        int marks=98;
+        char grade='A';
+        System.out.println("name is:"+name);
+        System.out.println("Subname is:"+subject);
+        System.out.println("marks is:"+marks);
+        System.out.println("Grade is:"+grade);
+    }
+}
