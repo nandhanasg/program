53a8339ea4b73496513b10c512ca323499016f08
nandhanasg <nandhana26112003>
2025-03-13 14:45:00 +0530

    ifstat

---
+++ ifstat.java
@@ -0,0 +1,17 @@
+public class ifstat
+{
+    public static void main(String[] args) {
+        {
+            int age=25;
+            if (age>=18)
+            {
+                System.out.println("major");
+            }
+            else{
+                System.out.println("minor");
+            }
+            }
+                
+            }
+        }
+    
