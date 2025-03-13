488a4c3d855538a86317f195ebd09e638d2d5aed
nandhanasg <nandhana26112003>
2025-03-13 15:24:58 +0530

    elseif

---
+++ elseif.java
@@ -0,0 +1,19 @@
+public class elseif
+{
+    public static void main(String[] args)
+    {
+        int mark=45;
+        if(mark>=50)
+        {
+            System.out.println("pass");
+        }
+          else  if(mark<=50)
+            {
+                System.out.println("fail");
+            }
+            else{
+                System.out.println("no marks");
+            }
+            }
+        }
+        
