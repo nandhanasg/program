c216409f0faadf4c958aa6f0c354f4c618843e58
nandhanasg <nandhana26112003>
2025-03-13 11:08:50 +0530

    opaddmul

---
+++ opaddmul.java
@@ -0,0 +1,11 @@
+public class opaddmul
+{
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int c=5;
+        int d=3;
+        System.out.println("Addition:"+(a+b));
+        System.out.println("Multiplication:"+c*d);
+    }
+}
