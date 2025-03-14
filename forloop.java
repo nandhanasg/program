937eed8d4f04c9b84e81f07eaafb0d4a7b8e69b2
nandhanasg <nandhana26112003>
2025-03-14 11:12:52 +0530

    forloop

---
+++ forloop.java
@@ -0,0 +1,15 @@
+public class forloop
+{
+    public static void main(String[] args)
+    {
+        int i=0;
+        for(i=0;i<=5;i++)
+        {
+            System.out.println("Nandhana");
+        }
+        }
+
+        
+        
+
+    }
