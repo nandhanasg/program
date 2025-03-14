909a80f9220d0f400f367e71f5d3c32b4c46caa5
nandhanasg <nandhana26112003>
2025-03-14 10:06:19 +0530

    string2

---
+++ string2.java
@@ -0,0 +1,20 @@
+
+
+public class string2
+{
+    public static void main(String[] args)
+    {
+        String input="hello";
+    
+        
+        if(input.matches("[a-zA-Z]+"))
+        {
+            System.out.println("The input is a string");
+        }
+        else{
+            System.out.println("The input is not a string");
+        }
+        }
+
+
+    }
