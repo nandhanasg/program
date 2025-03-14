5b0f9c2d9750b3c400435900925120b6ce51f2b9
nandhanasg <nandhana26112003>
2025-03-14 13:48:53 +0530

    array

---
+++ array.java
@@ -0,0 +1,12 @@
+public class array
+{
+    public static void main(String[] args) 
+        {
+            String[] veg ={"carrot","tomato","brinjal","potato","chilli"};
+            System.out.println(veg[1]);
+            System.out.println(veg[3]);
+            System.out.println(veg[4]);
+        }
+
+        }
+    
