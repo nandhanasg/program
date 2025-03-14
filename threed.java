0c56117a6a3b52f4497b969348b91350a072d4e3
nandhanasg <nandhana26112003>
2025-03-14 14:20:22 +0530

    threed

---
+++ threed.java
@@ -0,0 +1,10 @@
+public class threed
+{
+    public static void main(String[] args) 
+        {
+            int[] [] numbers={{1,2,3},{1,5,6},{4,8,2}};
+            System.out.println(numbers[2][1]);
+            System.out.println(numbers[1][0]);
+        }
+        }
+    
