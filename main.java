22bdc081c148644b56c988fc2bb20c846e6709a9
nandhanasg <nandhana26112003>
2025-03-14 12:18:48 +0530

    main

---
+++ main.java
@@ -0,0 +1,17 @@
+
+
+public class main
+{
+    static void mymethod()
+    {
+        System.out.println("welcome");
+
+    }
+    public static void main(String[] args) {
+        
+            mymethod();
+        }
+        }
+    
+
+    
