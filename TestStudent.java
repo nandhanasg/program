9dd2d7cb8e2f5f6597bf02d139d40ed1aea7ec87
nandhanasg <nandhana26112003>
2025-03-14 15:46:54 +0530

    TestStudent

---
+++ TestStudent.java
@@ -0,0 +1,18 @@
+class student
+{
+    int id;
+    String name;
+}
+public class TestStudent
+{
+    public static void main(String[] args) 
+        {
+            student2 s1=new student2();
+            s1.id=101;
+            System.out.println(s1.id);
+            student2 s2=new student2();
+            s2.name="Ram";
+            System.out.println(s2.name);
+        }
+        }
+    
