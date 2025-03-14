d1949a728fe93b2835f086fb4a6c0a6912788d75
nandhanasg <nandhana26112003>
2025-03-14 15:08:42 +0530

    forarray

---
+++ forarray.java
@@ -0,0 +1,15 @@
+public class forarray
+{
+    public static void main(String[] args) 
+        {
+            String[] fruits={"apple","mango","guava","Strawberry"};
+            for(int i=0;i<=3;i++)
+            {
+                System.out.println(fruits[i]);
+            }
+            
+
+        
+        
+        }
+    }
