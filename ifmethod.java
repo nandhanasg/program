0071110805142d7e57e31145e3053b4dae2f91eb
nandhanasg <nandhana26112003>
2025-03-14 13:32:39 +0530

    ifmethod

---
+++ ifmethod.java
@@ -0,0 +1,17 @@
+public class ifmethod
+{
+    static void mymethod()
+    {
+        int age=25;
+        if(age>=18)
+        {
+            System.out.println("can vote");
+        }
+    }
+    public static void main(String[] args) 
+        {
+            mymethod();
+
+        }
+    }
+    
