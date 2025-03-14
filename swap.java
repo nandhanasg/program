03865c10ef907ae2113c1ffdeb22adbbb6ee8cd8
nandhanasg <nandhana26112003>
2025-03-14 09:45:40 +0530

    swap

---
+++ swap.java
@@ -0,0 +1,21 @@
+public class swap
+{
+    public static void main(String[] args)
+    {
+        int a=20;
+        int b=10;
+        System.out.println("Before swapping:"+a);
+        System.out.println("Before swapping:"+b);
+        a=a-b;
+        b=a+b;
+        System.out.println("After swapping:"+ a);
+        System.out.println("After swapping:"+ b);
+    }
+}
+
+        
+
+
+         
+
+    
