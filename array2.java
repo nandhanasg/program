465c6ad311751ab8e04ca6ec72aef1a24b93fb71
nandhanasg <nandhana26112003>
2025-03-14 14:45:57 +0530

    array2

---
+++ array2.java
@@ -0,0 +1,11 @@
+
+
+public class array2
+{
+    public static void main(String[] args) {
+        {
+            String[] fruits={"apple","mango","guava"};
+            System.out.println(fruits[1]="orange");
+        }
+    }
+}
