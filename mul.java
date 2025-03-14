4d7aeb6c778ba541cae5347ba53369a40081c411
nandhanasg <nandhana26112003>
2025-03-14 13:18:10 +0530

    mul

---
+++ mul.java
@@ -0,0 +1,15 @@
+public class mul
+{
+    static void mulmethod()
+    {
+        int a=5;
+        int b=9;
+        System.out.println("multiplication:"+a*b);
+    }
+    public static void main(String[] args) 
+        {
+            mulmethod();
+
+        }
+    }
+    
