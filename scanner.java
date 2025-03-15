3935bba79563aa1ac3cfb05e05c06b06526f3771
nandhanasg <nandhana26112003>
2025-03-15 11:50:47 +0530

    scanner

---
+++ scanner.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class scanner{
+    public static void main(String[] args) {
+        {
+            Scanner a=new Scanner (System.in);
+            System.out.println("Enter username:");
+            String username=a.nextLine();
+            System.out.println("The username is:"+username);
+        }
+    }
+
+}
