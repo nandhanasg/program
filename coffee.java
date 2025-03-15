54003e3173520164db7f40275df9310e5f555385
nandhanasg <nandhana26112003>
2025-03-15 11:09:21 +0530

    coffee

---
+++ coffee.java
@@ -0,0 +1,38 @@
+public class coffee
+{
+    public static void main(String[] args) 
+        {
+            int choice=2;
+            switch(choice)
+            {
+                case 1:
+                    {
+                        System.out.println("Hot coffee");
+
+                    }
+                    break;
+                    case 2:
+                        {
+                            System.out.println("cold coffee");
+                        }
+                        break;
+                        case 3:
+                            {
+                                System.out.println("choco coffee");
+
+                            }
+                            break;
+                            case 4:
+                                {
+                                    System.out.println("bhadam coffee");
+
+                                }
+                                break;
+                                default:
+                                    {
+                                        System.out.println("no coffee");
+                                    }
+                                    break;
+            }
+        }
+    }
