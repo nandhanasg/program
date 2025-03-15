ffc93c2513b6d43f688bd610e090c694bcab03ff
nandhanasg <nandhana26112003>
2025-03-15 10:45:15 +0530

    customer

---
+++ customer.java
@@ -0,0 +1,14 @@
+public class customer
+{
+    public static void main(String[] args)
+    {
+        
+        int [] a={20,50,60,70,80};
+
+        for(int i=0;i<=5;i++)
+        {
+            System.out.println(a[i]);
+
+        }
+    }
+}
