1982b24856d022775c06c64709b5f6e229cd4000
nandhanasg <nandhana26112003>
2025-03-15 09:38:43 +0530

    swap2

---
+++ swap2.java
@@ -0,0 +1,15 @@
+public class swap2
+{
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        System.out.println("Before Swapping:"+a);
+        System.out.println("Before swapping:"+b);
+        int temp=a;
+        a=b;
+        b=temp;
+        System.out.println("After swapping:"+a);
+        System.out.println("After swapping:"+b);
+
+    }
+}
