87eb10a7e6b746d3310095f00cdc5fb93e13e742
nandhanasg <nandhana26112003>
2025-03-15 10:56:30 +0530

    price

---
+++ price.java
@@ -0,0 +1,20 @@
+
+
+public class price
+{
+    public static void main(String[] args) 
+        {
+
+            int [] cost={10,5,200,100};
+            System.out.println("The price of pen is:"+cost[0]);
+            System.out.println("The price of pencil is:"+cost[1]);
+            System.out.println("The price of bat is :"+cost[2]);
+            System.out.println("The price of ball is:"+cost[3]);
+
+
+
+
+            }
+
+        }
+    
