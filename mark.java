d8b76962e5c3b5c777388f3e4da21573a38261cc
nandhanasg <nandhana26112003>
2025-03-15 13:54:18 +0530

    mark

---
+++ mark.java
@@ -0,0 +1,20 @@
+public class mark
+{
+    public static void main(String[] args)
+    {
+        int marks=67;
+        if(marks>=90)
+        {
+            System.out.println("Grade is 'A'");
+        }
+         else  if(marks>=75)
+            {
+                System.out.println("Grade is 'B'");
+            }
+            else if(marks>=50)
+            {
+                System.out.println("Grade is 'C'");
+            }
+
+        }
+    }
